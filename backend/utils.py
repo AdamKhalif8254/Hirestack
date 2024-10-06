b6d8d223523de1b2253b9b515390a633c0a898ac
= <=>
2024-10-06 15:42:57 -0400

    Added Minsearch for optimized search functionalities

--- backend/utils.py
+++ backend/utils.py
@@ -3,6 +3,9 @@ from pymongo.errors import DuplicateKeyError
 import pandas as pd
 import re
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from minsearch import Index
 
 def connect_to_mongodb(connection_string, db_name, collection_name):
     client = MongoClient(connection_string)
@@ -100,4 +103,21 @@ def process_location(df):
     return df
 
 
+# Refresh the in-memory search index
+def refresh_index(collection):
+    # Fetch all job postings from the MongoDB collection
+    jobs = list(collection.find({}, {"_id": 0}))  # Fetch all documents, omit MongoDB's default `_id` field
 
+    # Initialize the search index
+    text_fields = ['title', 'description']
+    keyword_fields = ['location', 'city', 'company', 'job_type', 'industry']  # Adding fields that make sense for keyword matching
+    index = Index(text_fields=text_fields, keyword_fields=keyword_fields)
+
+    # Fit the index with the documents from MongoDB
+    index.fit(jobs)
+    return index
+
+# Perform a search using the in-memory index
+def search_jobs(index, query, filter_dict={}, boost_dict={}, num_results=10):
+    results = index.search(query, filter_dict=filter_dict, boost_dict=boost_dict, num_results=num_results)
+    return results
